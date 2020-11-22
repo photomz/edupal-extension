5aba977ce1087bbf7562cea28c3c10bf3c20a8c2
Markus Zhang <markuszhang8@gmail.com>
2020-11-22 16:09:41 +0800

    remove nod assets

--- config/webserver.js
+++ config/webserver.js
@@ -1,9 +1,9 @@
-var WebpackDevServer = require("webpack-dev-server"),
-  webpack = require("webpack"),
-  config = require("../webpack.config"),
-  env = require("./env"),
-  path = require("path"),
-  CopyWebpackPlugin = require("copy-webpack-plugin");
+const WebpackDevServer = require('webpack-dev-server');
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const config = require('../webpack.config');
+const env = require('./extensionEnv');
 
 // Webpack uses this path to fetch the manifest + hot update chunks.
 // Not setting it for content scripts or scripts injected into client
@@ -14,21 +14,21 @@ config.output.publicPath = `http://127.0.0.1:${env.PORT}/`;
 // Replace the manifest transformer so the new content security policy allows
 // the public path.
 
-var copyPluginIndex = config.plugins.findIndex(
-  p => p instanceof CopyWebpackPlugin
+const copyPluginIndex = config.plugins.findIndex(
+  (p) => p instanceof CopyWebpackPlugin
 );
 if (copyPluginIndex !== -1) {
   config.plugins[copyPluginIndex] = new CopyWebpackPlugin(
     [
       {
-        from: "./manifest.json",
-        transform: function(content) {
-          var manifest = JSON.parse(content.toString());
-          var content_security_policy =
-            (manifest.content_security_policy
-              ? manifest.content_security_policy + "; "
-              : "") +
-            `script-src 'self' ${config.output.publicPath}; object-src 'self'`;
+        from: './manifest.json',
+        transform(content) {
+          const manifest = JSON.parse(content.toString());
+          const content_security_policy = `${
+            manifest.content_security_policy
+              ? `${manifest.content_security_policy}; `
+              : ''
+          }script-src 'self' ${config.output.publicPath}; object-src 'self'`;
 
           // generates the manifest file using the package.json informations
           return Buffer.from(
@@ -37,34 +37,34 @@ if (copyPluginIndex !== -1) {
                 description: process.env.npm_package_description,
                 version: process.env.npm_package_version,
                 ...manifest,
-                content_security_policy
+                content_security_policy,
               },
               null,
               2
             )
           );
-        }
-      }
+        },
+      },
     ],
     {
-      copyUnmodified: true
+      copyUnmodified: true,
     }
   );
 }
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 
-var server = new WebpackDevServer(compiler, {
+const server = new WebpackDevServer(compiler, {
   hot: true,
-  contentBase: path.join(__dirname, "../build"),
+  contentBase: path.join(__dirname, '../build'),
   sockPort: env.PORT,
   headers: {
-    "Access-Control-Allow-Origin": "*"
+    'Access-Control-Allow-Origin': '*',
   },
   disableHostCheck: true,
-  host: "127.0.0.1",
-  transportMode: "ws",
-  writeToDisk: true
+  host: '127.0.0.1',
+  transportMode: 'ws',
+  writeToDisk: true,
 });
 
 server.listen(env.PORT);
