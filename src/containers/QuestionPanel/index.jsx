73dc8e911022e307fea47b814da5f87577a03841
Markus Zhang <markuszhang8@gmail.com>
2020-12-02 19:04:17 +0800

    Placeholder component for no questions

--- src/containers/QuestionPanel/index.jsx
+++ src/containers/QuestionPanel/index.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import LazyLoad from 'react-lazyload';
 import styled from 'styled-components';
+import MuiTypography from '@material-ui/core/Typography';
 
 import Carousel from './Carousel';
 import { questionIds } from '../../logic/question';
 
 const Wrapper = styled.div``;
 
+const H3 = styled(MuiTypography)`
+  margin-top: ${({ theme }) => theme.spacing(4)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const QuestionPanel = () => {
   const qids = useRecoilValue(questionIds);
+
+  if (!qids.length) return <H3 variant="h5">No questions yet!</H3>;
 
   return (
     <Wrapper id="edupal-questionPanel">
